027c89369bb63a382b83e43aaf3b662aa3cef53a
Rhys <rhysapfelbaum@gmail.com>
2023-06-05 13:45:33 +0100

    Update

--- script.js
+++ script.js
@@ -2,17 +2,72 @@
 var gSystem;
 
 // Global 'SystemCore' object which has the Core API functions.
-var gSystemCore;                 
-
-var trackEventDescription = {};  
-var trackEventInstance = {};
+var gSystemCore;
 
 var pauseSnapshot = {};
 var pauseSnapshotDescription = {};
 
-var tapeSounds = {};
-var tapeSoundsDescription = {};
+var playButtonSFX;
+var currentTrack;
+var currentTrackIndex;
+var rainEvent;
 
+var tracknames = [
+    'aquarium',
+    'rough',
+    'echo',
+    'snooze',
+    'lazy'
+];
+
+var tracklist = [];
+
+class SingleInstanceEvent {
+    description = null;
+    instance = null;
+    constructor(system, path) {
+        let outval = {};
+        CHECK_RESULT( system.getEvent(path, outval) );
+        this.description = outval.val;
+    }
+
+    get isLoaded() {
+        return this.instance != null;
+    }
+
+    async load() {
+        let outval = {};
+        CHECK_RESULT( this.description.createInstance(outval) );
+        this.instance = outval.val;
+    }
+
+    unload() {
+        // Mark the event instance for destruction
+        this.instance.release();
+
+        // Point instance to null
+        this.instance = null;
+    }
+
+    // Loads the event, plays it once, and immediately unloads it
+    oneShot() {
+        this.load();
+        this.instance.start();
+        this.instance.release();
+        this.instance = null;
+    }
+}
+
+class Track {
+    name;
+    displayName;
+    event = null;
+
+    constructor(system, name) {
+        this.name = name;
+        this.event = new SingleInstanceEvent(system, 'event:/Tracks/' + name);
+    }
+}
 
 // FMOD global object which must be declared to enable 'main' and 'preRun' and then call
 // the constructor function.
@@ -59,15 +114,10 @@ function main() {
     var outval = {};
     var result;
     
-    console.log("Creating FMOD System object\n");
     
     // Create the system and check the result
     result = FMOD.Studio_System_Create(outval);
     CHECK_RESULT(result);
-    
-    console.log("grabbing system object from temporary and storing it\n");
-    
-    // Take out our System object
     gSystem = outval.val;
     
     result = gSystem.getCoreSystem(outval);
@@ -77,26 +127,19 @@ function main() {
     
     // Optional.  Setting DSP Buffer size can affect latency and stability.
     // Processing is currently done in the main thread so anything lower than 2048 samples can cause stuttering on some devices.
-    console.log("set DSP Buffer size.\n");
     result = gSystemCore.setDSPBufferSize(2048, 2);
     CHECK_RESULT(result);
     
     // Optional.  Set sample rate of mixer to be the same as the OS output rate.
     // This can save CPU time and latency by avoiding the automatic insertion of a resampler at the output stage.
-    console.log("Set mixer sample rate");
     result = gSystemCore.getDriverInfo(0, null, null, outval, null, null);
     CHECK_RESULT(result);
     result = gSystemCore.setSoftwareFormat(outval.val, FMOD.SPEAKERMODE_DEFAULT, 0)
     CHECK_RESULT(result);
     
-    console.log("initialize FMOD\n");
-    
     // 1024 virtual channels
     result = gSystem.initialize(1024, FMOD.STUDIO_INIT_NORMAL, FMOD.INIT_NORMAL, null);
     CHECK_RESULT(result);
-    
-    // Starting up your typical JavaScript application loop
-    console.log("initialize Application\n");
     
     initApplication();
     
@@ -118,19 +161,26 @@ function initApplication() {
 
     loadBank('Master.bank');
     loadBank('Master.strings.bank');
-    console.log('load event');
-    CHECK_RESULT( gSystem.getEvent("event:/Rough", trackEventInstance) );
-    CHECK_RESULT( trackEventInstance.val.createInstance(trackEventInstance) );
-    
-    CHECK_RESULT( trackEventInstance.val.start() );
-
 
     CHECK_RESULT( gSystem.getEvent('snapshot:/Paused', pauseSnapshot));
     CHECK_RESULT( pauseSnapshot.val.createInstance(pauseSnapshot) );
+    
 
-    CHECK_RESULT( gSystem.getEvent('event:/tapeStop', tapeSounds));
-    CHECK_RESULT( tapeSounds.val.createInstance(tapeSounds) );
+    playButtonSFX = new SingleInstanceEvent(gSystem, 'event:/SFX/tapeStop');
+    rainEvent = new SingleInstanceEvent(gSystem, 'event:/Ambiences/Rain');
 
+    rainEvent.load();
+
+    tracknames.forEach((name) => {
+        tracklist.push(new Track(gSystem, name));
+    });
+
+    // Load the first track in the paused state
+    currentTrackIndex = 0;
+    currentTrack = tracklist[currentTrackIndex];
+    currentTrack.event.load();
+    currentTrack.event.instance.start();
+    document.querySelector('#current-track-name').innerHTML = currentTrack.name;
     setPauseState(true);
 }
 
@@ -139,42 +189,94 @@ function updateApplication() {
     // Update FMOD
     gSystem.update();
 
-    var intensityFinal = {};
-    
+    // Pause logic
+    let intensityFinal = {};
     CHECK_RESULT( pauseSnapshot.val.getParameterByName('Intensity', {}, intensityFinal) );
     if ((intensityFinal.val >= 100) && (! isPaused())) {
-        trackEventInstance.val.setPaused(true);
+        currentTrack.event.instance.setPaused(true);
     }
-    CHECK_RESULT( trackEventInstance.val.setParameterByName('Sampled', document.querySelector('#sampled').value / 100, false) );
-    
-    trackEventInstance.val.setParameterByName('Synthesized', document.querySelector('#synthesized').value / 100, false);
+    //currentTrack.event.instance.setParameterByName('Synthesized', document.querySelector('#synthesized').value / 100, false);
 }
 
 function isPaused() {
-    let paused = {};
-    let pauseSnapshotPlayback = {};
+    let outval = {};
+    let paused;
+    let pauseSnapshotPlayback;
 
-    CHECK_RESULT( trackEventInstance.val.getPaused(paused) );
-    CHECK_RESULT( pauseSnapshot.val.getPlaybackState(pauseSnapshotPlayback) );
-
-    if (paused.val) return true;
-    return pauseSnapshotPlayback.val != FMOD.STUDIO_PLAYBACK_STOPPED;
+    CHECK_RESULT( currentTrack.event.instance.getPaused(outval) );
+    paused = outval.val;
+    CHECK_RESULT( pauseSnapshot.val.getPlaybackState(outval) );
+    pauseSnapshotPlayback = outval.val;
+    if (paused) return true;
+    return pauseSnapshotPlayback != FMOD.STUDIO_PLAYBACK_STOPPED;
 }
 
 function setPauseState(state) {
     if (state) {
-        // Pause the track
-
         CHECK_RESULT( pauseSnapshot.val.start() );
-        trackEventInstance.val.setPaused(true);
     } else {
         // Play the track
-        trackEventInstance.val.setPaused(false);
+        currentTrack.event.instance.setPaused(false);
         pauseSnapshot.val.stop(FMOD.STUDIO_STOP_ALLOWFADEOUT);
     }
 }
 
+function nextTrack() {
+    playButtonSFX.oneShot();
+    currentTrack.event.instance.stop(FMOD.STUDIO_STOP_ALLOWFADEOUT);
+    currentTrack.event.unload();
+    currentTrackIndex = (currentTrackIndex + 1) % tracklist.length;
+    currentTrack = tracklist[currentTrackIndex];
+    currentTrack.event.load();
+
+    updateTrackSliders();
+
+    currentTrack.event.instance.start();
+    document.querySelector('#current-track-name').innerHTML = currentTrack.name;
+
+}
+
+function toggleAmbience(type) {
+    if (type != 'rain') return;
+
+    let playBackState = {};
+
+    playButtonSFX.oneShot();
+    updateRainAmount();
+    CHECK_RESULT( rainEvent.instance.getPlaybackState(playBackState) );
+    if (playBackState.val == FMOD.STUDIO_PLAYBACK_STOPPED) {
+        // Turn rain on
+        rainEvent.instance.start();
+        document.querySelector('#rain-toggle').children[0].style['background-color'] = 'rgb(211, 40, 40)';
+    } else {
+        // Turn rain off
+        rainEvent.instance.stop(FMOD.STUDIO_STOP_ALLOWFADEOUT);
+        document.querySelector('#rain-toggle').children[0].style['background-color'] = 'rgb(48, 48, 48)';
+        
+    }
+}
+
+function updateTrackSliders() {
+    let grit = document.querySelector('#grit').value / 100;
+    let synths = document.querySelector('#synths').value / 100;
+    let chops = document.querySelector('#chops').value / 100;
+    let vocals = document.querySelector('#vocals').value / 100;
+
+    currentTrack.event.instance.setParameterByName('Sampled', grit, false);
+    currentTrack.event.instance.setParameterByName('Synthesized', synths, false);
+    currentTrack.event.instance.setParameterByName('Chopped', chops, false);
+
+
+
+}
+
+function updateRainAmount() {
+    let rainAmount = document.querySelector('#rain-amount').value / 100;
+    rainEvent.instance.setParameterByName('RainAmount', rainAmount, false);
+    
+}
+
 function togglePause() {
-    tapeSounds.val.start();
+    playButtonSFX.oneShot();
     setPauseState(!isPaused());
 }
