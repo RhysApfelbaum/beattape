ac8ebe89106e7cc14a22c53627f986d1ddde4cbc
RhysApfelbaum <rhysapfelbaum@gmail.com>
2025-11-21 16:17:53 +0000

    Remove console.log

--- colorscheme.ts
+++ colorscheme.ts
@@ -77,7 +77,6 @@ Object.keys(referenceColors).forEach((key) => {
     const color = hexToRgb(
         referenceColors[key as keyof typeof referenceColors],
     );
-    // console.log(color, colorDistance(referenceMean, color))
 });
 
 const nearestDistance = (imageBuffer: Buffer, distance: number): RGB => {
