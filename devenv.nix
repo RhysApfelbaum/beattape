2fe28a21a5172c8aeaaca2d8be0a613598a603cb
RhysApfelbaum <rhysapfelbaum@gmail.com>
2025-12-22 13:09:32 +0000

    Remove extra deps

--- devenv.nix
+++ devenv.nix
@@ -12,16 +12,9 @@ in {
     git
     tailwindcss-language-server
     ffmpeg
-    # emscripten
     python3
-    # gettext
-    # wget
-    # libtool
-    # automake
-    # autoconf
   ];
 
-  # https://devenv.sh/languages/
   languages.javascript = {
     enable = true;
     bun.enable = true;
@@ -29,36 +22,4 @@ in {
     npm.enable = true;
   };
 
-  # https://devenv.sh/processes/
-  # processes.cargo-watch.exec = "cargo-watch";
-
-  # https://devenv.sh/services/
-  # services.postgres.enable = true;
-
-  # https://devenv.sh/scripts/
-  scripts.hello.exec = ''
-    echo hello from $GREET
-  '';
-
-  enterShell = ''
-    hello
-    git --version
-  '';
-
-  # https://devenv.sh/tasks/
-  # tasks = {
-  #   "myproj:setup".exec = "mytool build";
-  #   "devenv:enterShell".after = [ "myproj:setup" ];
-  # };
-
-  # https://devenv.sh/tests/
-  enterTest = ''
-    echo "Running tests"
-    git --version | grep --color=auto "${pkgs.git.version}"
-  '';
-
-  # https://devenv.sh/git-hooks/
-  # git-hooks.hooks.shellcheck.enable = true;
-
-  # See full reference at https://devenv.sh/reference/options/
 }
